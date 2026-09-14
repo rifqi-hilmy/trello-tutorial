d08ca3dfd408d3624087822c0f78f17914ed400a
Muhamad Rifqi Hilmy Apriadi <muhamadrifqi240@gmail.com>
2026-09-14 18:09:58 +0700

    [feat] add useAction abstraction

---
+++ lib/create-safe-action.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+export type FieldErrors<T> = {
+  [K in keyof T]?: string[];
+};
+
+export type ActionState<Tinput, TOutput> = {
+  fieldErrors?: FieldErrors<Tinput>;
+  error?: string | null;
+  data?: TOutput;
+};
+
+export const createSafeAction = <TInput, TOuput>(
+  schema: z.Schema<TInput>,
+  handler: (validateData: TInput) => Promise<ActionState<TInput, TOutput>>,
+) => {
+  return async (data: TInput): Promise<ActionState<TInput, TOutput>> => {
+    const validationResult = schema.safeParse(data);
+    if (!validationResult.success) {
+      return {
+        fieldErrors: validationResult.error.flatten()
+          .fieldErrors as FieldErrors<TInput>,
+      };
+    }
+
+    return handler(validationResult.data);
+  };
+};
